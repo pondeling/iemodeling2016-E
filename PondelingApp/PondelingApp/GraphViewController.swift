29b7d27e9a31f9104551717957d99151c872ea68
incho <akisyoudesuga.nanika@gmail.com>
2017-01-08 00:10:21 +0900

    Cleate homeView

---
+++ PondelingApp/PondelingApp/GraphViewController.swift
@@ -0,0 +1,25 @@
+//
+//  graph.swift
+//  PondelingApp
+//
+//  Created by 上原優里奈 on 2017/01/05.
+//  Copyright © 2017年 上原優里奈. All rights reserved.
+//
+
+import UIKit
+
+
+class GraphViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
